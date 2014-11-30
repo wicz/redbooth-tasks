1cbfc3a6cc7dbb7372699480d2912a078f9d47ad
Vinicius Horewicz <vinicius@horewi.cz>
2014-11-30 12:52:52 +0100

    Fix authentication spec
    After a successful authentication, we redirect the use to /tasks. So
    stub the request to the API to avoid a spec failure.

--- spec/features/sessions/create_spec.rb
+++ spec/features/sessions/create_spec.rb
@@ -1,11 +1,16 @@
 # encoding: utf-8
 require "rails_helper"
+require "webmock/rspec"
 require_spec_support "authentication"
 
 describe "Sign in with Redbooth" do
+  before do
+    stub_request(:get, /.*redbooth.*/) .to_return(body: "")
+  end
+
   it do
-    visit root_path
-    click_on "Sign in"
+    visit     root_path
+    click_on  "Sign in"
 
     expect(page).to have_content("My Tasks")
   end
